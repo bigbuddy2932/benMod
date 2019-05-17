d95992d3982e0b2e1fb43d386b11306824c43850
Benjamin Roeder <renboeder@gmail.com>
2019-05-17 16:52:55 -0400

    Added micro assembler item and entity

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -18,5 +18,100 @@ data:extend({
     fuel_category = "chemical",
     fuel_value = "50GJ",
     stack_size = 10
+  },
+  {
+	type = "assembling-machine",
+	name = "micro-assembler",
+	icon = "__benMod__/graphics/entity/micro-assembler.png",
+	icon_size = 32,
+	flags = {"placeable-neutral", "placeable-player", "player-creation"},
+	minable = {mining_time = 0.1, result = "micro-assembler"},
+	max_health = 200,
+	corpse = "small-remnants",
+	collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
+	selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+	animation = {
+      layers = {
+        {
+          filename = "__benMod__/graphics/entity/micro-assembler.png",
+          frame_count = 1,
+          height = 128,
+          line_length = 8,
+          priority = "high",
+          shift = {0, 0},
+		  scale = 0.2,
+          width = 128
+        }
+	  }
+	},
+	alert_icon_scale = 0.5,
+	module_specification = {
+      module_slots = 4
+    },
+	allowed_effects = {
+      "consumption",
+      "speed",
+      "productivity",
+      "pollution"
+    },
+	open_sound = {
+      filename = "__base__/sound/machine-open.ogg",
+      volume = 0.85
+    },
+	close_sound = {
+      filename = "__base__/sound/machine-close.ogg",
+      volume = 0.75
+    },
+    resistances = {
+      {
+        percent = 70,
+        type = "fire"
+      }
+	},
+	vehicle_impact_sound = {
+        filename = "__base__/sound/car-metal-impact.ogg",
+        volume = 0.65
+    },
+    working_sound = {
+      apparent_volume = 1.5,
+      idle_sound = {
+        filename = "__base__/sound/idle1.ogg",
+        volume = 0.6
+      },
+      sound = {
+        {
+          filename = "__base__/sound/assembling-machine-t1-1.ogg",
+          volume = 0.8
+        },
+        {
+          filename = "__base__/sound/assembling-machine-t1-2.ogg",
+          volume = 0.8
+        }
+      }
+    },
+	crafting_categories = {
+      "basic-crafting",
+      "crafting",
+      "advanced-crafting"
+	},
+	ingredient_count = 1,
+	crafting_speed = 1.1,
+	dying_explosion = "medium-explosion",
+	energy_source = {
+	  emissions_per_minute = 2,
+	  type = "electric",
+	  usage_priority = "secondary-input"
+	},
+	energy_usage = "140kW"
+  },
+  {
+    type = "item",
+    name = "micro-assembler",
+    icon = "__benMod__/graphics/entity/micro-assembler.png",--WIP
+	icon_size = 128,
+    subgroup = "production-machine",
+    order = "a-[assembling-machine-1]",
+	place_result = "micro-assembler",
+    stack_size = 50
   }
 })
--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -5,7 +5,7 @@ data:extend({
     icon = "__benMod__/graphics/icons/bentonium.png",
 	icon_size = 32,
     subgroup = "intermediate-product",
-    order = "a[uranium-processing]-c[bentonium-make]",
+    order = "a[uranium-processing]",
     energy_required = 5,
     enabled = true,
     ingredients =
@@ -22,7 +22,7 @@ data:extend({
     icon = "__benMod__/graphics/icons/ben-cell.png",
 	icon_size = 32,
     subgroup = "intermediate-product",
-    order = "a[bentonium-make]-c[uranium-fuel-cell]",
+    order = "a[bentonium]",
     energy_required = 10,
     enabled = true,
     ingredients =
@@ -32,5 +32,24 @@ data:extend({
     },
     result = "ben-cell",
     result_count = 2
+  },
+  {
+    type = "recipe",
+    name = "micro-assembler",
+    icon = "__benMod__/graphics/entity/micro-assembler.png",--WIP
+	icon_size = 128,
+    subgroup = "production-machine",
+    order = "a-[assembling-machine-1]",
+    energy_required = 0.5,
+    enabled = true,
+    ingredients =
+    {
+      {"iron-plate", 9},
+	  {"electronic-circuit", 3},
+	  {"iron-gear-wheel", 5},
+      {"bentonium", 1}
+    },
+    result = "micro-assembler",
+    result_count = 1
   }
 })
